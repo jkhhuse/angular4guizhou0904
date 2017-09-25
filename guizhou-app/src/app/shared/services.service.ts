e3aa1ab50dfebd78b9183995fc9ec2d57a8be94a
zhenghao <zhenghao35791@gmail.com>
2017-09-25 10:19:29 +0800

    应用商城appstore更新后台接口数据调用

--- guizhou-app/src/app/shared/services.service.ts
+++ guizhou-app/src/app/shared/services.service.ts
@@ -16,7 +16,11 @@ export class ServicesService {
         } else if (moduleName === 'service') {
             //  return this.http.get('/api' + '/app1.0/groups/1/services?isPublic=1').map(res => res.json());
             return this.http.get('/apiService' + '/groups/1/services?isPublic=' + tabName).map(res => res.json());
-        }/* else if (moduleName === 'serviceDetail') {
+        } else if (moduleName === 'application') {
+            //  return this.http.get('/api' + '/app1.0/groups/1/services?isPublic=1').map(res => res.json());
+            return this.http.get('/apiApp' + '/groups/1/applications').map(res => res.json());
+        }
+        /* else if (moduleName === 'serviceDetail') {
             //  服务详情里面，tabName字段传入的是服务id，serviceId
             return this.http.get('/apiService' + '/groups/1/services/' + tabName + '/instances').map(res => res.json());
         }*/
