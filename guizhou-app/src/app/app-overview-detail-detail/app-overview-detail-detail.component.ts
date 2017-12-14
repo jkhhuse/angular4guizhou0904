daf7dd4b7dd892bff3764427bca59b868041661e
zhenghao <zhenghao@zhenghaodeMacBook-Pro.local>
2017-12-14 19:32:34 +0800

    完成服务实例详情页
    完成服务实例详情页

--- guizhou-app/src/app/app-overview-detail-detail/app-overview-detail-detail.component.ts
+++ guizhou-app/src/app/app-overview-detail-detail/app-overview-detail-detail.component.ts
@@ -3,6 +3,7 @@ import {Observable} from "rxjs/Observable";
 import {ActivatedRoute} from '@angular/router';
 import {Http} from "@angular/http";
 import {environment} from "../../environments/environment";
+
 @Component({
   selector: 'app-app-overview-detail-detail',
   templateUrl: './app-overview-detail-detail.component.html',
@@ -15,9 +16,64 @@ export class AppOverviewDetailDetailComponent implements OnInit {
   private instanceId: String;
   private moduleName: String;
   private instanceDetail: any;
+  tabs = [
+    {
+      index: 1,
+      name: '服务地址'
+    },
+    {
+      index: 2,
+      name: '容器实例'
+    },
+    {
+      index: 3,
+      name: '监控'
+    },
+    {
+      index: 4,
+      name: '日志'
+    },
+    {
+      index: 5,
+      name: '环境变量'
+    },
+    {
+      index: 6,
+      name: '配置文件'
+    },
+    {
+      index: 7,
+      name: '存储卷'
+    }
+  ];
+  // 容器实例表格
+  table5Title = [
+    {
+      index: 1,
+      name: '实例',
+    },
+    {
+      index: 3,
+      name: 'IP地址',
+    },
+    {
+      index: 4,
+      name: '容器IP',
+    },
+    {
+      index: 5,
+      name: '创建时间',
+    },
+    {
+      index: 6,
+      name: '操作',
+    }
+  ];
+
   getServiceInstanceDetail(instanceId, moduleName): Observable<any[]> {
     return this.http.get(environment.apiService + '/apiService' + '/service-instances/' + instanceId + '/modules/' + moduleName).map(res => res.json());
   }
+
   constructor(private routeInfo: ActivatedRoute, private http: Http) {
   }
 
