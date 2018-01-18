d466d29ba5e9cee9549aa534da3a39b6e64d8396
zhenghao <zhenghao@cmss.chinamobile.com>
2018-01-18 17:35:17 +0800

    添加缺失文件

---
+++ guizhou-app/src/app/opera-monitor/opera-monitor.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+@Component({
+    selector: 'app-opera-monitor',
+    templateUrl: './opera-monitor.component.html',
+    styleUrls: ['./opera-monitor.component.css']
+})
+export class OperaMonitorComponent implements OnInit {
+
+    private title = '运维监控';
+
+    tabs = [
+        {
+            name   : '平台监控',
+            index: 'platform'
+        },
+        {
+            name   : '运维监控',
+            content: 'Content of Tab Pane 2'
+        }
+    ];
+
+    constructor(private _router: Router) { }
+
+    ngOnInit() {
+    }
+
+    /**
+     * 切换标签页
+     * @param tab 标签页
+     */
+    changeToService() {
+        this._router.navigate(['operaMonitor/platform']);
+    }
+
+    changeToPlatform() {
+        this._router.navigate(['operaMonitor/service']);
+    }
+
+}
