22ddcdbcca3e80cbe4c0d6e3820249b3c9ff615f
codingzhou <gitcodingzhou@163.com>
2018-02-07 23:44:17 +0800

    bug fix
    修复bug：
    贵州线上，无法拿到开通的端口号

--- guizhou-app/src/environments/environment.ts
+++ guizhou-app/src/environments/environment.ts
@@ -1,18 +1,18 @@
 
-export const environment = {
-    production: false,
-    name: '贵州v3.5线上环境',
-    apiService: 'http://135.10.143.20:18032',
-    apiApp: 'http://135.10.143.20:18031',
-    apiConfig: 'http://135.10.143.20:18037',
-    api: 'http://135.10.143.20:18180',
-    apiAlauda: 'http://135.10.143.20:18039/apiJakiro/v1',
-    apiOP: 'http://135.10.143.20:17080/bdoc/v2',
-    bomp: 'http://135.10.143.20:8080/api',
-    pass: 'http://135.10.143.20:18035/opsOverview',
-    adminGroupId: '0',
-    namespace: 'alauda'
-};
+// export const environment = {
+//     production: false,
+//     name: '贵州v3.5线上环境',
+//     apiService: 'http://135.10.143.20:18032',
+//     apiApp: 'http://135.10.143.20:18031',
+//     apiConfig: 'http://135.10.143.20:18037',
+//     api: 'http://135.10.143.20:18180',
+//     apiAlauda: 'http://135.10.143.20:18039/apiJakiro/v1',
+//     apiOP: 'http://135.10.143.20:17080/bdoc/v2',
+//     bomp: 'http://135.10.143.20:8080/api',
+//     pass: 'http://135.10.143.20:18035/opsOverview',
+//     adminGroupId: '0',
+//     namespace: 'alauda'
+// };
 /*export const environment = {
     production: false,
     name: '贵州v3.0线上环境',
@@ -26,7 +26,7 @@ export const environment = {
     adminGroupId: '0',
     namespace: 'alauda'
 };*/
-/*export const environment = {
+export const environment = {
     production: false,
     name: 'op联调提测环境',
     apiService: 'http://10.132.49.122:18032',
@@ -40,5 +40,5 @@ export const environment = {
     groupId: '2',
     adminGroupId: '0',
     namespace: 'alauda'
-};*/
+};
 
