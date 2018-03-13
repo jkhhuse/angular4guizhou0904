b4dbffb74d75da48879324477ac0d619c24d13bf
zhenghao <zhenghao@zhenghaodeMacBook-Pro.local>
2018-03-13 18:32:27 +0800

    新增灰度状态列表，新增灰度详情表单
    新增灰度状态列表，新增灰度详情表单

--- guizhou-app/src/app/app-overview-detail/app-overview-detail.component.ts
+++ guizhou-app/src/app/app-overview-detail/app-overview-detail.component.ts
@@ -1,12 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import {Http} from "@angular/http";
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {RandomUserService} from "../shared/random-user.service";
 import {NzNotificationService} from 'ng-zorro-antd';
 import {ServicesService} from "../shared/services.service";
 import {environment} from "../../environments/environment";
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import  { GrayModifyReqBody } from './app-overview-detail.model';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import {GrayListDetail, GrayModifyReqBody} from './app-overview-detail.model';
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-app-overview-detail',
@@ -14,10 +15,22 @@ import  { GrayModifyReqBody } from './app-overview-detail.model';
   styleUrls: ['./app-overview-detail.component.css']
 })
 export class AppOverviewDetailComponent implements OnInit {
+  _grayDataSet: GrayListDetail[];
   validateForm: FormGroup;
+  detailForm: FormGroup;
+  grayDetailDataset: any;
+  detailID: string;
   _isSpinning = false;
-  userId:string;
-  userName:string;
+  userId: string;
+  userName: string;
+  // 详情原始数据
+  oldServiceName: string;
+  lbName: string;
+  newServiceName: string;
+  portName: string;
+  hostIpArray = [];
+  eqIpArray = [];
+  rangeIpArray = [];
   // 版本升级选择框
   versionOptions = [];
   selectedVersion;
@@ -32,7 +45,8 @@ export class AppOverviewDetailComponent implements OnInit {
   private appInstanceDetail: any;
   maskClosable = true;
   isModifyModalVisible = false;
-  isFormDisabled = false;
+  isDetailModalVisible = false;
+  isFormDisabled = true;
   tabs = [
     {
       index: 1,
@@ -96,22 +110,13 @@ export class AppOverviewDetailComponent implements OnInit {
   _sortName = null;
   _sortValue = null;
   _dataSet = [];
-  _grayDataSet = [
-    {
-      createTime : '123123',
-      version1 : '1',
-      version2 : '2',
-      status : 'running',
-      rules_intro : '128.0.0.1',
-      microservices : 'fufu',
-    }
-  ];
   copyData = [...this._dataSet];
 
   changeTabName(tabName): void {
     this.tabName = tabName;
-    if(this.tabName==='grayStatus') {
-
+    if (this.tabName === 'grayStatus') {
+      // 点击灰度状态tab，获取灰度list
+      this.getGrayDataSet();
     } else {
       this.refreshData();
     }
@@ -195,21 +200,22 @@ export class AppOverviewDetailComponent implements OnInit {
     }, 3000);
   };
 
-  putNewVersion () {
+  putNewVersion() {
     this.userId = this.servicesService.getUserId();
     console.log('选择userId：' + this.userId);
     this.http.put(environment.apiApp + '/apiApp' + '/groups/' + this.servicesService.getCookie('groupID') + '/application-instances/' + this.instanceId, {
       'applicationVersion': this.selectedVersion,
       'updateUserId': this.userId
     }).subscribe(response => {
-      console.log('这是response', response);
+        console.log('这是response', response);
 
-    },
+      },
       err => {
         console.log(err._body);
         this.createNotification('error', '升级应用失败', err._body);
       });
   }
+
   handleCancel = (e) => {
     /*this.userId = this.servicesService.getUserId();
     console.log('选择userId：' + this.userId);
@@ -217,30 +223,32 @@ export class AppOverviewDetailComponent implements OnInit {
     console.log(e);
     this.isVisible = false;
   };
+
   // 获取流
   getAppVersions(appName) {
     return this.http.get(environment.apiApp + '/apiApp' + '/groups/' + this.servicesService.getCookie('groupID') + '/applications/' + appName + '/versions').map(res => res.json());
   }
+
   // 获取初始数据
   getInitData() {
     this._randomUser.getAppInstanceDetail(this.instanceId).subscribe((data) => {
         this.appInstanceDetail = data;
         // 部署实例的名称
-        if(data.app) {
+        if (data.app) {
           this.appName = data.app.appName;
           // 订阅流, 获取当前应用下部署实例的版本
           this.getAppVersions(this.appName).subscribe((data2) => {
             this.instanceVersions = data2;
             // 添加版本升级选择框内的option数据
-            if(this.instanceVersions) {
+            if (this.instanceVersions) {
               // 每次拼接版本值之前，清空版本数据
               this.versionOptions = [];
-              for(let j=0; j<this.instanceVersions.length;j++) {
+              for (let j = 0; j < this.instanceVersions.length; j++) {
                 let temp = this.instanceVersions[j].version;
                 this.versionOptions.push(temp);
               }
               // 如果版本option里面有数据，把第一个值赋值为初始版本值
-              if(this.versionOptions.length > 0) {
+              if (this.versionOptions.length > 0) {
                 this.selectedVersion = this.versionOptions[0];
               }
             }
@@ -254,13 +262,113 @@ export class AppOverviewDetailComponent implements OnInit {
     );
   }
 
-  showUpdateModal() {
+  // 获取灰度状态tab中的列表流
+  getGrayDataSetRX(instanceId) {
+    return this.http.get(environment.apiApp + '/apiApp' + '/application-instances/' + instanceId + '/gray-updates').map(res => res.json());
+  }
+
+  // 获取灰度状态tab中的列表数据
+  getGrayDataSet() {
+    this.getGrayDataSetRX(this.instanceId).subscribe((data) => {
+        this._grayDataSet = data;
+        // console.log(this._grayDataSet);
+      },
+      err => {
+        console.log(err._body);
+        this.createNotification('error', '获取灰度列表失败', err._body);
+      }
+    );
+  }
+
+  // 获取灰度状态详情流
+  getGrayDetailRX(detailID) {
+    return this.http.get(environment.apiApp + '/apiApp' + '/gray-updates/' + detailID + '/rules').map(res => res.json());
+  }
+
+  // 获取灰度状态详情
+  getGrayDetail(detailID) {
+    this.getGrayDetailRX(detailID).subscribe((data) => {
+        this.hostIpArray = [];
+        this.eqIpArray = [];
+        this.rangeIpArray = [];
+        this.grayDetailDataset = data[0];
+        // console.log(this.grayDetailDataset);
+
+        this.oldServiceName = this.grayDetailDataset.microservice1.microserviceName;
+        this.newServiceName = this.grayDetailDataset.microservice2.microserviceName;
+        this.lbName = this.grayDetailDataset.lbName;
+        this.portName = this.grayDetailDataset.port;
+        if (this.grayDetailDataset.dsl && this.grayDetailDataset.dsl.length > 0) {
+          // 去掉最后一个字符）
+          console.log(this.grayDetailDataset.dsl.substring(0, this.grayDetailDataset.dsl.length - 1));
+          // 按照（字符 拆分
+          let grayIpArray = this.grayDetailDataset.dsl.substring(0, this.grayDetailDataset.dsl.length - 1).split('(');
+          for (let i = 0; i < grayIpArray.length; i++) {
+            if (grayIpArray[i].indexOf('IN') >= 0) {
+              let HOSTTemp = grayIpArray[i].replace(')', '').split(' ');
+              // console.log('HOSTTemp: ' + HOSTTemp[2]);
+              this.hostIpArray.push(HOSTTemp[2]);
+              console.log('hostIpArray: ' + this.hostIpArray);
+            } else if (grayIpArray[i].indexOf('EQ') >= 0) {
+              let EQTemp = grayIpArray[i].replace(')', '').split(' ');
+              // console.log('EQTemp: ' + EQTemp[2]);
+              this.eqIpArray.push(EQTemp[2]);
+              console.log('eqIpArray: ' + this.eqIpArray);
+            } else if (grayIpArray[i].indexOf('RANGE') >= 0) {
+              let RANGETemp = grayIpArray[i].replace(')', '').split(' ');
+              // console.log('RANGETemp: ' + RANGETemp[2] + ',' + RANGETemp[3]);
+              this.rangeIpArray.push(RANGETemp[2] + ' ~ ' + RANGETemp[3]);
+              console.log('rangeIpArray: ' + this.rangeIpArray);
+            }
+          }
+        }
+
+        this.validateForm = this.fb.group({
+          oldServiceName: this.oldServiceName,
+          lbName: this.lbName,
+          newServiceName: this.newServiceName,
+          portName: this.portName,
+          select1: ['固定IP'],
+          select2: ['IP段限制'],
+          value1: this.eqIpArray,
+          value2: this.rangeIpArray,
+        });
+      },
+      err => {
+        console.log(err._body);
+        this.createNotification('error', '灰度状态详情失败', err._body);
+      }
+    );
+  }
+
+  showUpdateModal(detailID) {
     this.isModifyModalVisible = true;
+    if (detailID == '' || detailID == 'undefined') {
+      this.createNotification('error', '获取灰度详情ID错误', '获取灰度详情ID错误');
+    } else {
+      this.detailID = detailID;
+      this.getGrayDetail(this.detailID);
+    }
+  }
+
+  showDetailModal(detailID) {
+    this.isDetailModalVisible = true;
+    if (detailID == '' || detailID == 'undefined') {
+      this.createNotification('error', '获取灰度详情ID错误', '获取灰度详情ID错误');
+    } else {
+      this.detailID = detailID;
+      this.getGrayDetail(this.detailID);
+    }
   }
 
   // 获取表单内对应name的formcontrol
   getFormControl(name) {
-    return this.validateForm.controls[ name ];
+    return this.validateForm.controls[name];
+  }
+
+  // 取消查看灰度策略详情
+  cancelDetail() {
+    this.isDetailModalVisible = false;
   }
 
   // 取消提交灰度策略
@@ -271,7 +379,7 @@ export class AppOverviewDetailComponent implements OnInit {
   // 提交更新灰度策略
   postUpdate($event) {
     for (const i of Object.keys(this.validateForm.controls)) {
-      this.validateForm.controls[ i ].markAsDirty();
+      this.validateForm.controls[i].markAsDirty();
     }
     let isValid = true;
     for (const value of Object.values(this.validateForm.value)) {
@@ -294,11 +402,19 @@ export class AppOverviewDetailComponent implements OnInit {
 
   ngOnInit() {
     this.validateForm = this.fb.group({
-      portName       : [ null, [ Validators.required ] ]
+      oldServiceName: this.oldServiceName,
+      lbName: this.lbName,
+      newServiceName: this.newServiceName,
+      portName: this.portName,
+      select1: ['固定IP'],
+      select2: ['IP段限制'],
+      value1: this.eqIpArray,
+      value2: this.rangeIpArray,
     });
+    this.detailForm = this.fb.group({});
     this.instanceId = this.routeInfo.snapshot.params['instanceId'];
     this.refreshData();
     this.getInitData();
+    this.getGrayDataSet();
   }
-
 }
--- guizhou-app/src/app/app-overview-detail/app-overview-detail.model.ts
+++ guizhou-app/src/app/app-overview-detail/app-overview-detail.model.ts
@@ -1,3 +1,15 @@
+export class GrayListDetail {
+  'id':string;
+  'appInstance1Id':string;
+  'appInstance2Id':string;
+  'status':string;
+  'createTime': any;
+  'version1': string;
+  'version2': string;
+  'rules_intro': string;
+  'microservices': string;
+}
+
 export class GrayModifyReqBody {
   portName: number;
 }
