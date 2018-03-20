78060e1578e4128ed74bc3dc010abb5fdc57daec
jkhhuse <jkhhuse@126.com>
2018-03-20 15:20:33 +0800

    修改拦截器、解决拦截器与Service中http循环依赖问题。

--- guizhou-app/src/app/util/request/request.interceptor.ts
+++ guizhou-app/src/app/util/request/request.interceptor.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Injectable, forwardRef, Injector } from '@angular/core';
+import { HttpHeaders, HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-
+import { CookieService } from 'angular2-cookie';
 
 @Injectable()
 export class RequestInterceptor implements HttpInterceptor {
+
+
+  constructor(private _cookieService: CookieService) {
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // const userName = this.auth.getUserName();
     const customReq = req.clone({
-      withCredentials: false
+      // withCredentials: true,
+      headers: new HttpHeaders().append('BDOC-User', this._cookieService.get('userName'))
     });
 
     return next
