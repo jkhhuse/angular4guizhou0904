08440fa274f291828e716a1e88070bc689592bfe
zhenghao <zhenghao35791@gmail.com>
2018-04-20 10:12:39 +0800

    jira 970+新增贵州测试环境ip地址
    jira 970+新增贵州测试环境ip地址

--- guizhou-app/src/app/app-release/app-release.component.ts
+++ guizhou-app/src/app/app-release/app-release.component.ts
@@ -142,7 +142,7 @@ export class AppReleaseComponent implements OnInit {
       placeholder: '请输入应用名称',
       validation: [Validators.required, Validators.pattern(/^[a-zA-Z]([-a-zA-Z0-9]*[a-zA-Z0-9])?$/), Validators.maxLength(20)],
       styles: {
-        'width': '400px'
+        'width': '400px',
       }
     },
     {
@@ -281,6 +281,7 @@ export class AppReleaseComponent implements OnInit {
         defaultValue: this.appName,
         validation: [Validators.required, Validators.pattern(/^[a-zA-Z]([-a-zA-Z0-9]*[a-zA-Z0-9])?$/), Validators.maxLength(20)],
         inputDisabled: true,
+        disabled: true,
         styles: {
           'width': '400px'
         }
