bf44f28ddeffd32115b43f933edf189bd1af227c
zhenghao <zhenghao35791@gmail.com>
2018-05-16 16:00:44 +0800

    1.面包屑导航 2.平台日志增加搜索 3.项目组优化，去掉id后缀
    1.面包屑导航
    2.平台日志增加搜索
    3.项目组优化，去掉id后缀

--- guizhou-app/src/app/group-select/group-select.component.ts
+++ guizhou-app/src/app/group-select/group-select.component.ts
@@ -1,100 +1,80 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {ServicesService} from "../shared/services.service";
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {ServicesService} from '../shared/services.service';
 import {NzNotificationService} from 'ng-zorro-antd';
 
 @Component({
-    selector: 'app-group-select',
-    templateUrl: './group-select.component.html',
-    styleUrls: ['./group-select.component.css']
+  selector: 'app-group-select',
+  templateUrl: './group-select.component.html',
+  styleUrls: ['./group-select.component.css']
 })
 export class GroupSelectComponent implements OnInit {
-    @Output()
-    groupidHandler: EventEmitter<string> = new EventEmitter();
-    firstGroupName: string;
-    firstGroupID: number;
-    groupList: any;
-    selectGroup: any;
-    // groupId: string;
-    // groupName: string;
-    changeGroup(data) {
-        console.log('data: ' + data);
-        let temp: any;
-        if (!!data.toString().split('_')) {
-            temp = data.toString().split('_');
-            // this.groupId = temp[1];
-            // this.groupName = temp[0];
-            // console.log('temp1: ' + temp[1]);
-            if (this.servicesService.getCookie('groupID') === temp[1]) {
-                // 如果传入的新groupid和cookie里面已经保存的一样，不需要刷新页面
-            } else {
-                // 更新groupID 和 groupName
-                this.servicesService.setCookie('groupID', temp[1]);
-                this.servicesService.setCookie('groupName', data);
-                console.log('changeGroup SETcookie: ' + this.servicesService.getCookie('groupID'));
-                // 修改当前的第一个groupid和groupname
-                this.firstGroupName = this.servicesService.getCookie('groupName');
-                this.firstGroupID = this.servicesService.getCookie('groupID');
-                // 如果更新groupid，向父组件传送一个子组件的string对象
-                this.groupidHandler.emit(temp[1]);
-            }
+  @Output()
+  groupidHandler: EventEmitter<string> = new EventEmitter();
+  firstGroupName: string;
+  firstGroupID: any;
+  groupList: any;
+  selectGroup: any;
+
+  changeGroup(group) {
+    if (group && group.id) {
+      if (this.servicesService.getCookie('groupID') === group.id) {
+        // 如果传入的新groupid和cookie里面已经保存的一样，不需要刷新页面
+      } else {
+        // 更新groupID 和 groupName
+        this.servicesService.setCookie('groupID', group.id);
+        this.servicesService.setCookie('groupName', group.name);
+        console.log('changeGroup SETcookie: ' + this.servicesService.getCookie('groupID'));
+        // 修改当前的第一个groupid和groupname
+        this.firstGroupName = this.servicesService.getCookie('groupName');
+        this.firstGroupID = this.servicesService.getCookie('groupID');
+        // 如果更新groupid，向父组件传送一个子组件的string对象
+        this.groupidHandler.emit(group.id);
+      }
+    } else {
+      // 如果group取不到？，获取不到groupid，cookie传入空
+      this.servicesService.setCookie('groupID', '');
+    }
+  }
+
+  createNotification = (type, title, content) => {
+    this._notification.create(type, title, content);
+  }
+
+  constructor(private servicesService: ServicesService, private _notification: NzNotificationService) {
+  }
+
+  ngOnInit() {
+    // 订阅op的group流
+    this.servicesService.getGroupList().subscribe((data) => {
+        // 过滤出需要的数据，拼接成一个array
+        this.groupList = this.servicesService.getGroupNameList(data);
+        if (this.groupList.length > 0) {
+          // 加载项目选择框的时候，默认把第一个group作为默认项目组
+          this.firstGroupName = this.groupList[0].name;
+          this.firstGroupID = this.groupList[0].id;
+          if ((this.servicesService.getCookie('groupID') === '' )
+            || (typeof(this.servicesService.getCookie('groupID')) === 'undefined')) {
+            // 如果cookie的值是空的，往cookie中传入第一个id值
+            this.servicesService.setCookie('groupID', this.firstGroupID);
+            this.servicesService.setCookie('groupName', this.firstGroupName);
+            this.selectGroup = this.firstGroupName;
+            // 向外传出默认的第一个groupid
+            this.groupidHandler.emit(this.firstGroupID);
+          } else {
+            // 如果cookie的值不是空的，从已有的cookie中选择出旧的groupID
+            this.groupidHandler.emit(this.servicesService.getCookie('groupID'));
+            // 如果已经有存在的group，从cookie中取得选中的groupName
+            this.selectGroup = this.servicesService.getCookie('groupName');
+          }
         } else {
-            // 如果group取不到？，获取不到groupid，cookie传入空
-            this.servicesService.setCookie('groupID', '');
+          this.createNotification('error', '项目组为空', '项目组为空');
         }
-    }
-
-    createNotification = (type, title, content) => {
-        this._notification.create(type, title, content);
-    }
-
-    constructor(private servicesService: ServicesService, private _notification: NzNotificationService) {
-    }
-
-    ngOnInit() {
-        /*// 如果groupid是空的，去cookie里面取得默认值
-        if (this.groupid = 'undefined') {
-            console.log('groupid = \'undefined\': ' + this.groupid);
-
-            this.groupid = this.servicesService.getCookie('groupID');
-            console.log('groupid = \'2222222\': ' + this.groupid);
-
-        }
-        console.log('groupList: ' + this.groupList);
-        console.log('groupID 默认: ' + this.groupid);
-        console.log('groupID cookie: ' + this.servicesService.getCookie('groupID'));*/
-
-        // 订阅op的group流
-        this.servicesService.getGroupList().subscribe((data) => {
-            // 过滤出需要的数据，拼接成一个array
-            this.groupList = this.servicesService.getGroupNameList(data);
-            // this.groupList  = [ 'aaa_1', 'testd_2', 'BDOC-TEST-11_5', 'GGGGGGG_10', 'GROUP2_9', 'test111_8', 'asd_7'];
-            console.log('groupList: ' + this.groupList);
-            console.log('groupList[0]: ' + this.groupList[0]);
-            // 加载项目选择框的时候，默认把第一个group作为默认项目组
-            const temp = this.groupList[0].split('_');
-            this.firstGroupName = this.groupList[0];
-            this.firstGroupID = temp[1];
-            console.log('firstGroupName: ' + this.firstGroupName);
-            console.log('cookie groupID: ' + this.servicesService.getCookie('groupID'));
-            if ((this.servicesService.getCookie('groupID') === '' ) || (typeof(this.servicesService.getCookie('groupID')) === 'undefined')) {
-                // 如果cookie的值是空的，往cookie中传入第一个id值
-                this.servicesService.setCookie('groupID', this.firstGroupID);
-                this.servicesService.setCookie('groupName', this.firstGroupName);
-                this.selectGroup = this.firstGroupName;
-                // 向外传出默认的第一个groupid
-                this.groupidHandler.emit(temp[1]);
-            } else {
-                // 如果cookie的值不是空的，从已有的cookie中选择出旧的groupID
-                this.groupidHandler.emit(this.servicesService.getCookie('groupID'));
-                // 如果已经有存在的group，从cookie中取得选中的groupName
-                this.selectGroup = this.servicesService.getCookie('groupName');
-            }
-        },
-            err => {
-                console.log(err);
-                this.createNotification('error', '获取项目组信息失败', err._body);
-            });
-    }
+      },
+      err => {
+        console.log(err);
+        this.createNotification('error', '获取项目组信息失败', err._body);
+      });
+  }
 }
 
 /*
